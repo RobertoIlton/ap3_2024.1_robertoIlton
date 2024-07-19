ecf94daea6e9c9edb23721f18615e40d9115c4ca
Eshelyvitoria <eshelyvitoria05@gmail.com>
2024-07-19 20:37:04 -0300

    Criado a pasta da aula 2
    fazer tipos primitivos no java

---
+++ unidade1/gabarito_aula2/TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package br.com.roberto.aula2;
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        byte codigoByte = 112;
+        System.out.println("");
+        System.out.println(codigoByte);
+    }
+}
