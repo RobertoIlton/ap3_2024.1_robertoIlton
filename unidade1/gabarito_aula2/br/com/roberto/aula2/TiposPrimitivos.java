7ee7949487d97d783f892f026be206e02c30e058
Eshelyvitoria <robeertoilton2@hotmail.com>
2024-07-19 21:24:14 -0300

    Atualização da variaveis
    adicionado (byte, short, int, long, float, double, boolean, char).
    OBS: String deve ser em S maiusculo

--- unidade1/gabarito_aula2/br/com/roberto/aula2/TiposPrimitivos.java
+++ unidade1/gabarito_aula2/br/com/roberto/aula2/TiposPrimitivos.java
@@ -6,5 +6,37 @@ public class TiposPrimitivos {
         byte codigoByte = 112;
         System.out.println("");
         System.out.println(codigoByte);
+
+        short codigoShort = 114;
+        System.out.println("");
+        System.out.println(codigoShort);
+
+        int codigoInt = 10;
+        System.out.println("");
+        System.out.println(codigoInt);
+
+        long codigoLong = 213;
+        System.out.println("");
+        System.out.println(codigoLong);
+
+        float codigoFloat = 111;
+        System.out.println("");
+        System.out.println(codigoFloat);
+
+        double codigoDouble = 2;
+        System.out.println("");
+        System.out.println(codigoDouble);
+
+        boolean codigoBoolean = true;
+        System.out.println();
+        System.out.println(codigoBoolean);
+
+        char codigoChar = 'R';
+        System.out.println("");
+        System.out.println(codigoChar);
+
+        String codigoString = "roberto";
+        System.out.println("");
+        System.out.println(codigoString);
     }
 }
